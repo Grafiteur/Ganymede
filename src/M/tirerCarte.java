9f671bf52e9de8800bfee71d85ca2792967bdf5e
simon <simon.dalponte@gmail.com>
2018-11-21 16:26:14 +0100

    y me manque plus que les ecouteur a placer sur les image

--- src/M/tirerCarte.java
+++ src/M/tirerCarte.java
@@ -9,17 +9,27 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class tirerCarte extends Application {
+
+
     public static void main(String[] args) {
 
         Application.launch(args);
-
-
     }
     @Override
     public void start(Stage primaryStage) {
         Jeu jeu = new Jeu();
         System.out.println("apres");
         jeu.init();
+
+        System.out.println("-------------------------------------------");
+        for (VaisseauColon v: jeu.getAffichage()) {
+            System.out.println(v.getImg());
+        }
+
+
+
+
+
 
     }
 }
